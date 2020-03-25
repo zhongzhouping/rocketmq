188fec0caca3be7b8efdd5613f882db41f5099a5
zhongzhouping <zhongzhouping@foxmail.com>
2020-03-25 17:33:44 +0800

    feat：添加启动测试相关代码

--- namesrv/src/test/java/org/apache/rocketmq/namesrv/NameServerInstanceTest.java
+++ namesrv/src/test/java/org/apache/rocketmq/namesrv/NameServerInstanceTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.rocketmq.namesrv;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.rocketmq.common.namesrv.NamesrvConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
 import org.junit.After;
@@ -43,5 +44,19 @@ public class NameServerInstanceTest {
             nameSrvController.shutdown();
         }
         //maybe need to clean the file store. But we do not suggest deleting anything.
+    }
+
+    public static void main(String[] args) throws Exception {
+        // NamesrvConfig配置
+        final NamesrvConfig namesrvConfig = new NamesrvConfig();
+        // NettyServerConfig 配置
+        final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+        // 设置监听端口
+        nettyServerConfig.setListenPort(9876);
+        NamesrvController namesrvController = new NamesrvController(namesrvConfig, nettyServerConfig);
+        namesrvController.initialize();
+        namesrvController.start();
+
+        Thread.sleep(DateUtils.MILLIS_PER_DAY);
     }
 }
